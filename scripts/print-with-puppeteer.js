df082e47a784316264bfd7a16b98d6f9f4311556
Gimpneek <colin@gimpneek.com>
2019-02-11 20:48:47 +0000

    adding FRESH Resume files

---
+++ scripts/print-with-puppeteer.js
@@ -0,0 +1,23 @@
+const puppeteer = require("puppeteer");
+const path = require("path");
+const filePath = path.resolve(__dirname, '../out/cv.html');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(`file:///${filePath}`);
+  await page.pdf({
+    path: "out/CV.pdf",
+    format: "A4",
+    printBackground: true,
+    displayHeaderFooter: false,
+    margin: {
+      top: "10mm",
+      left: "10mm",
+      right: "10mm",
+      bottom: "10mm"
+    }
+  });
+
+  await browser.close();
+})();
